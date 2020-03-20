18a96c22ec36e9ce1a527de520c9a5c412540fb0
lordbastor <lordbastor@gmail.com>
2020-03-20 22:29:54 +0200

    Adds debug default value for environ

--- calculator/settings.py
+++ calculator/settings.py
@@ -14,7 +14,7 @@ import os
 
 import environ
 
-env = environ.Env()
+env = environ.Env(DEBUG=(bool, False))
 env.read_env(env.str("ENV_PATH", ".env"))
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -27,7 +27,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env("DEBUG", False)
+DEBUG = env("DEBUG")
 
 ALLOWED_HOSTS = ["bastor.pythonanywhere.com"]
 
